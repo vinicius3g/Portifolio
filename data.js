4024ec5126010e61e5117b45ae272bc9b5a7c0ee
vinicius.paixao <vinicius.paixao@acct.global>
2020-08-20 17:04:32 -0300

    videos em destaque

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: "2alg7MQ6_sI",
+        title: "Formulario animado com JS puro e CSS animation",
+        duration: "51 min",
+        price: "free",
+        featured: true
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Formulario animado com JS puro e CSS animation",
+        duration: "51 min",
+        price: "free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Formulario animado com JS puro e CSS animation",
+        duration: "51 min",
+        price: "free",
+        featured: true
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Formulario animado com JS puro e CSS animation",
+        duration: "51 min",
+        price: "free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Formulario animado com JS puro e CSS animation",
+        duration: "51 min",
+        price: "free",
+        featured: true
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Formulario animado com JS puro e CSS animation",
+        duration: "51 min",
+        price: "free"
+    }
+]
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const videos = require('./data')
+
+
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure('views', {
+    express: server
+})
+
+server.get("/", function (req, res) {
+    const about = {
+        avatar_url: "https://avatars0.githubusercontent.com/u/58923275?s=460&u=e60a6a2e08854dce1a3cf06c61da8bf4832170b9&v=4",
+        name: "Vinícius Dos Santos Paixao",
+        role: "Desenvolvedor Web",
+        description: "Programador web, com foco no full-stack",
+        links: [
+            { name: "Github", url: "https://github.com/vinicius3g" },
+            { name: "Linkedin", url: "https://github.com/vinicius3g" },
+            { name: "Facebook", url: "https://github.com/vinicius3g" }
+        ]
+    }
+    return res.render("about", { about })
+})
+server.get("/portifolio", function (req, res) {
+    return res.render("portifolio", { items: videos })
+})
+
+server.listen(5000, function () {
+    console.log('server is running')
+})
